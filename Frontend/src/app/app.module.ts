18054e3a9342b46d8d08e1ddf15de580ebc105fe
Kevin Malomba <malombakevin@gmail.com>
2023-03-07 14:52:34 +0300

    feat and style;Nvaavbar, footer- Adds footer and navbar html and css files

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./Components/home/home.component";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { FooterComponent } from "./Components/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NavbarComponent,
+        FooterComponent
+    ]
 })
 export class AppModule { }
