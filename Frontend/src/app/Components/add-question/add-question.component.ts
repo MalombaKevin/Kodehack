6be9a04944e1f38b4b618709f7d13d873a1b2ded
Kevin Malomba <malombakevin@gmail.com>
2023-03-07 11:29:41 +0300

    feat;components- Adds project components having html.css and ts files

---
+++ Frontend/src/app/Components/add-question/add-question.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-question',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-question.component.html',
+  styleUrls: ['./add-question.component.css']
+})
+export class AddQuestionComponent {
+
+}
---
+++ Frontend/src/app/Components/debug-page/debug-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-debug-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './debug-page.component.html',
+  styleUrls: ['./debug-page.component.css']
+})
+export class DebugPageComponent {
+
+}
