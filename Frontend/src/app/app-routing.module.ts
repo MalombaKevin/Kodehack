6943b5565e0e71f6b01ae458862d4781e80423c0
Kevin Malomba <malombakevin@gmail.com>
2023-03-08 19:04:26 +0300

    feat;profile-Adds profile html and css

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DebugPageComponent } from './Components/debug-page/debug-page.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 const routes: Routes = [
 
   {path:'', component:LandingPageComponent},
-  {path:'kodehack/home', component:HomeComponent}
+  {path:'home', component:HomeComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'debug', component:DebugPageComponent}
 
 ];
 
